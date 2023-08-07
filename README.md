9f4e1c1950bfd4305fa2de6ef546402836c7946f
Yusuke Kawamoto <novogrammer@gmail.com>
2023-08-07 18:53:10 +0900

    three.js

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vite Three Voronoi Gems
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,66 @@
 import './style.scss'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+import * as THREE from "three";
+
+const appElement=document.querySelector<HTMLDivElement>('#app')!;
+appElement.innerHTML = `
 <canvas id="renderCanvas"></canvas>
 `;
+
+interface Size{
+  width:number;
+  height:number;
+};
+
+function getSize():Size{
+  const width=appElement.clientWidth;
+  const height=appElement.clientHeight;
+  return {
+    width,
+    height,
+  }
+}
+
+
+const renderCanvasElement=document.querySelector<HTMLCanvasElement>("#renderCanvas")!;
+
+
+const scene = new THREE.Scene();
+const size=getSize()
+const camera = new THREE.PerspectiveCamera( 75, size.width / size.height, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+  canvas:renderCanvasElement,
+});
+
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+
+function onResize(){
+  const size=getSize();
+  camera.aspect=size.width / size.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(size.width,size.height);
+}
+window.addEventListener("resize",onResize);
+onResize();
+
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
+animate();
 
