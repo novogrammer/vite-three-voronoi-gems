a697e12f2851686b474325350c888f0444c021b7
Yusuke Kawamoto <novogrammer@gmail.com>
2023-08-08 11:05:45 +0900

    利用者として不要な情報を整理

--- src/voronoi.d.ts
+++ src/voronoi.d.ts
@@ -10,7 +10,7 @@ declare module "voronoi" {
   class Halfedge{
     site:Site;
     edge:Edge;
-    angle?:number;
+    angle:number;
     getStartpoint():Vertex;
     getEndpoint():Vertex;
   }
@@ -18,7 +18,6 @@ declare module "voronoi" {
     site:Site;
     halfedges:Halfedge[];
     closeMe:boolean;
-    constructor(site:Site);
   }
   class Edge{
     lSite:Site;
@@ -26,13 +25,6 @@ declare module "voronoi" {
     va:Vertex;
     vb:Vertex;
   }
-  // class Beachsection{
-  //   site:Site;
-  //   rbPrevious?:number;
-  // }
-  // class CircleEvent{
-
-  // }
   interface Bbox{
     xl:number;
     xr:number;
@@ -47,23 +39,6 @@ declare module "voronoi" {
     execTime:number;
   }
   class Voronoi{
-    vertices:Vertex[]|null;
-    edges:Edge[]|null;
-    cells:Cell[]|null;
-    // toRecycle:[]|null;
-    // createHalfedge(edge:Edge,lSite:Site,rSite:Site):Halfedge;
-    // createVertex(x:number,y:number):Vertex;
-    // createEdge(lSite:Site,rSite:Site,va:Vertex,vb:Vertex):Edge;
-    // createBorderEdge(lSite:Site,va:Vertex,vb:Vertex):Edge;
-    // setEdgeStartpoint(edge:Edge,lSite:Site,rSite:Site,vertex:Vertex):void;
-    // setEdgeEndpoint(edge:Edge,lSite:Site,rSite:Site,vertex:Vertex):void;
-    // createBeachsection(site:Site):Beachsection;
-    // leftBreakPoint(arc:Beachsection,directrix:number):number;
-    // rightBreakPoint(arc:Beachsection,directrix:number):number;
-    // detachBeachsection(beachsection:Beachsection):void;
-    // removeBeachsection(beachsection:Beachsection):void;
-    // addBeachsection(site:Site):void;
-
     compute(sites:Vertex[],bbox:Bbox):Diagram;
   }
   export = Voronoi;
